fbf82e2fe0d3757e7e2c29f77c643a546f5fd184
Valentín Turco <128485313+valennt@users.noreply.github.com>
2023-11-07 18:15:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,4 @@
-//Create a web server
-//Create a route to get all comments
-//Create a route to get a single comment
-//Create a route to create a comment
-//Create a route to update a comment
-//Create a route to delete a comment
-//Create a route to get all comments of a post
-//Create a route to get all comments of a user
+
 
 const express = require('express');
 const router = express.Router();
