4f50c65fc00a21d785c19f3d712d49d026634e5d
Valentín Turco <128485313+valennt@users.noreply.github.com>
2023-11-07 18:17:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,6 @@
 
 
-const express = require('express');
-const router = express.Router();
-const Comment = require('../models/Comment');
-const Post = require('../models/Post');
-const { route } = require('./users');
+
 
 
 
