00c705437fe355fe9c99418de835adf7fe2c04f2
Valentín Turco <128485313+valennt@users.noreply.github.com>
2023-11-07 18:14:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,61 +1,23 @@
-//Create a web server that can respond to requests for /hello and /goodbye
-//with the appropriate text.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
-//Use the fs module to create a server that reads and serves up the index.html
-//file from the previous exercise.
+//Create a web server
+//Create a route to get all comments
+//Create a route to get a single comment
+//Create a route to create a comment
+//Create a route to update a comment
+//Create a route to delete a comment
+//Create a route to get all comments of a post
+//Create a route to get all comments of a user
 
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+const { route } = require('./users');
 
-var server = http.createServer(function(request, response) {
-  if (request.url === '/hello') {
-    response.writeHead(200, {
-      'Content-Type': 'text/plain'
-    });
-    response.write('hello world');
-    response.end();
-  } else if (request.url === '/goodbye') {
-    response.writeHead(200, {
-      'Content-Type': 'text/plain'
-    });
-    response.write('goodbye world');
-    response.end();
-  } else if (request.url === '/index.html') {
-    fs.readFile('index.html', function(err, data) {
-      if (err) {
-        response.writeHead(404);
-        response.write('Not Found!');
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/html'
-        });
-        response.write(data.toString());
-      }
-      response.end();
-    });
-  } else {
-    response.writeHead(404);
-    response.write('Not Found!');
-    response.end();
-  }
-});
 
-server.listen(3000);
+
+
+
+
 
 
 
